e3c7a27525b7daf7582a04c9c4cf7563f2d66894
mako314 <119079347+mako314@users.noreply.github.com>
2023-05-03 11:02:08 -0400

    add first class functions

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(postRun){
+  postRun();
+}
+
+
+function returnsANamedFunction(){
+    return function potato(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){
+
+    }
+}
+
+//this lab was way harder than I thought for no reason
